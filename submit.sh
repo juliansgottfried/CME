0e991f62eba395f32567b2790357128c816464bb
Julian Gottfried <juliansg@princeton.edu>
2026-07-11 19:20:54 -0700

    update

--- submit.sh
+++ submit.sh
@@ -4,7 +4,7 @@ sbatch \
     --nodes=5 \
     --ntasks-per-node=20 \
     --mem=7G \
-    --time=15:00:00 \
+    --time=24:00:00 \
     --output=/scratch/users/jgottf/CME/output/%j.out \
     --error=/scratch/users/jgottf/CME/output/%j.out \
     --partition=normal,hns \
