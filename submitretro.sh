1159b88c072bb74effbc3dc51591fb0d056eba72
Julian Gottfried <juliansg@princeton.edu>
2026-07-26 00:18:24 -0700

    update

--- submitretro.sh
+++ submitretro.sh
@@ -3,7 +3,7 @@ ml julia
 sbatch \
     --nodes=8 \
     --ntasks-per-node=25 \
-    --mem=64G \
+    --mem=128G \
     --time=03:00:00 \
     --output=/scratch/users/jgottf/CME/output/%j.out \
     --error=/scratch/users/jgottf/CME/output/%j.out \
