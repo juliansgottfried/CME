178e13a2a8d3581bf09b0e6c4ffed286f034bd9a
Julian Gottfried <juliansg@princeton.edu>
2026-08-12 11:35:28 -0700

    update

--- runlocal.jl
+++ runlocal.jl
@@ -5,6 +5,10 @@ using JLD2, DelimitedFiles
 f = 0:0.01:0.99
 R0 = 0:0.1:9.9
 
+α = 0:1
+f = 0:1
+R0 = 0:1
+
 J = 200
 G = 7000
 inter = 100
@@ -12,19 +16,20 @@ nbin = 50
 nN = 168
 
 for i in eachindex(R0)
-    i = 89
     counts = zeros(Int, nbin * nN, length(f) * length(α))
+    len = Int(G / inter)
+    all = zeros(Int, nN, J * len)
+    t = zeros(Int, nN)
     for j in eachindex(α)
         for k in eachindex(f)
             println("i: $i, j: $j, k: $k")
-            # all = sim.replication(J, G, inter, nN, α[j], f[k], R0[i])
-            # all[all .> nbin - 1] .= nbin - 1
-            # all .+= 1
+            Sim.replication!(all, J, G, inter, nN, len, t, α[j], f[k], R0[i])
+            all[all .> nbin - 1] .= nbin - 1
+            all .+= 1
             for u in 1:nN
-                # [counts[nbin * (u - 1) + l, length(f) * (j - 1) + k] += 1 for l in all[u, :]]
-                [counts[nbin * (u - 1) + l, length(f) * (j - 1) + k] += 1 for l in ones(Int, 100)]
+                [counts[nbin * (u - 1) + l, length(f) * (j - 1) + k] += 1 for l in all[u, :]]
             end
         end
     end
-    writedlm("JLresults/counts_$i.csv", counts, ',')
+    writedlm("JLresultstmp/counts_$i.csv", counts, ',')
 end
