59678a3a2263044ea70eb51a7ccfac05998cd99d
Julian Gottfried <juliansg@princeton.edu>
2026-08-14 12:06:06 -0700

    ok

--- submit4.sh
+++ submit4.sh
@@ -1,13 +1,13 @@
 ml julia
 
 sbatch \
-    --ntasks=3 \
+    --ntasks=100 \
     --cpus-per-task=1 \
     --mem-per-cpu=1G \
-    --time=01:00:00 \
+    --time=02:00:00 \
     --output=/scratch/users/jgottf/CME/output/%j.out \
     --error=/scratch/users/jgottf/CME/output/%j.out \
     --partition=normal,hns \
     --mail-type=ALL \
     --mail-user=juliansgottfried@gmail.com \
-    /scratch/users/jgottf/CME/run2.jl
+    /scratch/users/jgottf/CME/run4.jl
