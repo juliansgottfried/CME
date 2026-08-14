7df6fcefd012f62fe652826367dc00c18052544b
Julian Gottfried <juliansg@princeton.edu>
2026-08-14 14:12:50 -0700

    ok

---
+++ submit_slim4.sh
@@ -0,0 +1,13 @@
+ml julia
+
+sbatch \
+    --ntasks=100 \
+    --cpus-per-task=1 \
+    --mem-per-cpu=500M \
+    --time=04:00:00 \
+    --output=/scratch/users/jgottf/CME/output/%j.out \
+    --error=/scratch/users/jgottf/CME/output/%j.out \
+    --partition=normal,hns \
+    --mail-type=ALL \
+    --mail-user=juliansgottfried@gmail.com \
+    /scratch/users/jgottf/CME/run_slim4.jl
