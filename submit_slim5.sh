c57ca1c98f75af49979bc07d0d4daf832ca9e86b
Julian Gottfried <juliansg@princeton.edu>
2026-08-14 14:21:48 -0700

    ok

--- submit_slim5.sh
+++ submit_slim5.sh
@@ -3,7 +3,7 @@ ml julia
 sbatch \
     --ntasks=100 \
     --cpus-per-task=1 \
-    --mem-per-cpu=500M \
+    --mem-per-cpu=2G \
     --time=06:00:00 \
     --output=/scratch/users/jgottf/CME/output/%j.out \
     --error=/scratch/users/jgottf/CME/output/%j.out \
