f1b3590ef277a6b342dcb0c24a5c0b241377c972
Julian Gottfried <juliansg@princeton.edu>
2026-09-22 10:50:24 -0700

    ok

--- convert_b.jl
+++ convert_b.jl
@@ -15,7 +15,7 @@ pmap(1:114) do i
     counts = zeros(Float64, nN * length(R0) * length(π), nbin + 3)
     for j in 1:10
         println("$i, $j")
-        loop!(counts, i, j)
+        Helper.loop!(counts, i, j)
     end
     DelimitedFiles.writedlm("/scratch/users/jgottf/CME/results_marc_2_reshape/counts_$i.csv", counts, ',')
 end
