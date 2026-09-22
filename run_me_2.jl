ca37cdf26b2f8f24466a98ee54b0c4f68150e3ce
Julian Gottfried <juliansg@princeton.edu>
2026-09-21 21:50:19 -0700

    ok

--- run_me_2.jl
+++ run_me_2.jl
@@ -1,37 +1,42 @@
-include("simnb.jl")
-import StatsBase, DelimitedFiles
+#!/usr/bin/env julia
 
-μ = 97
-σ2 = 516
-λ = σ2 / μ - 1
+using Distributed, SlurmClusterManager
+addprocs(SlurmManager())
 
-R0 = (0:0.02:(2 - 0.02)) ./ 100
-π = 0:0.0001:(0.01 - 0.0001)
-ϕ = 0
+@everywhere include("/scratch/users/jgottf/CME/sim_me_2.jl")
+@everywhere import StatsBase, DelimitedFiles
 
-J = 100
-G = 50
-inter = 1
-nbin = 50
+@everywhere plugins = DelimitedFiles.readdlm("/scratch/users/jgottf/CME/plugins.csv", ',')
+@everywhere μ = plugins[:, 1]
+@everywhere λ = plugins[:, 2] ./ plugins[:, 1] .- 1
 
-nN = 160
+@everywhere R0 = (0:0.002:(0.2 - 0.002))
+@everywhere π = 0:0.0005:(0.05 - 0.0005)
+@everywhere ϕ = 0
 
-for i in eachindex(R0)
-    counts = zeros(Int, nbin * nN, length(π) * length(ϕ))
+@everywhere J = 100
+@everywhere G = 50
+@everywhere inter = 1
+@everywhere nbin = 50
+
+@everywhere nN = 168
+
+pmap(eachindex(μ)) do i
+    counts = zeros(Int, nbin * nN, length(R0) * length(π))
     len = Int(G / inter)
     all = zeros(Int, nN, J * len)
     t = zeros(Int, nN)
-    for j in eachindex(π)
-        for k in eachindex(ϕ)
+    for j in eachindex(R0)
+        for k in eachindex(π)
             println("i: $i, j: $j, k: $k")
-            β = R0[i] * (1 + λ * (1 + ϕ[k] * (1 - π[j])))
-            Sim.replication!(all, J, G, inter, nN, len, t, β, π[j], ϕ[k], μ, λ)
+            β = R0[j] * (1 + λ[i] * (1 + ϕ * (1 - π[k])))
+            Sim.replication!(all, J, G, inter, nN, len, t, β, π[k], ϕ, μ[i], λ[i])
             all[all .> nbin - 1] .= nbin - 1
             all .+= 1
             for u in 1:nN
-                counts[nbin * (u - 1) .+ (1:nbin), length(ϕ) * (j - 1) + k] .= StatsBase.counts(all[u, :], 1:nbin)
+                counts[nbin * (u - 1) .+ (1:nbin), length(π) * (j - 1) + k] .= StatsBase.counts(all[u, :], 1:nbin)
             end
         end
     end
-    DelimitedFiles.writedlm("results/counts_$(i).csv", counts, ',')
+    DelimitedFiles.writedlm("/scratch/users/jgottf/CME/results_me_2/counts_$(i).csv", counts, ',')
 end
--- submit.sh
+++ submit.sh
@@ -3,11 +3,11 @@ ml julia
 sbatch \
     --ntasks=114 \
     --cpus-per-task=1 \
-    --mem-per-cpu=80G \
-    --time=00:02:00 \
+    --mem-per-cpu=2G \
+    --time=00:30:00 \
     --output=/scratch/users/jgottf/CME/output/%j.out \
     --error=/scratch/users/jgottf/CME/output/%j.out \
     --partition=normal,hns \
     --mail-type=ALL \
     --mail-user=juliansgottfried@gmail.com \
-    /scratch/users/jgottf/CME/convert.jl
+    /scratch/users/jgottf/CME/run_me_2.jl
