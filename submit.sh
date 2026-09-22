f7d490c214722fdad4d7262f6b009ebee30b8068
Julian Gottfried <juliansg@princeton.edu>
2026-09-21 21:56:46 -0700

    ok

--- submit.sh
+++ submit.sh
@@ -3,8 +3,8 @@ ml julia
 sbatch \
     --ntasks=114 \
     --cpus-per-task=1 \
-    --mem-per-cpu=8G \
-    --time=00:05:00 \
+    --mem-per-cpu=16G \
+    --time=00:02:00 \
     --output=/scratch/users/jgottf/CME/output/%j.out \
     --error=/scratch/users/jgottf/CME/output/%j.out \
     --partition=normal,hns \
